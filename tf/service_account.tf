03d052233867c3fd78c6b3fe5f419afee3633678
Colin Simmons <colin.simmons@engineerbetter.com>
2019-05-30 16:24:29 +0100

    add tf for service account
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

---
+++ tf/service_account.tf
@@ -0,0 +1,27 @@
+variable "region" {}
+variable "gcp_creds" {}
+variable "project_id" {}
+
+provider "google" {
+  credentials = "${var.gcp_creds}"
+  project     = "${var.project_id}"
+  region      = "${var.region}"
+}
+
+resource "google_service_account" "concourse" {
+  account_id   = "concourse"
+  display_name = "Concourse"
+}
+
+resource "google_service_account_iam_member" "concourse-iam" {
+  service_account_id = "${google_service_account.concourse.name}"
+  role               = "roles/editor"
+}
+
+resource "google_service_account_key" "key" {
+  service_account_id = "${google_service_account.concourse.name}"
+}
+
+output "key" {
+  value = "${google_service_account_key.key.public_key}"
+}
