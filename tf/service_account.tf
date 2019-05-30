26c1895bdb91bf12f8b9a194005be3d875d8b6c7
Colin Simmons <colin.simmons@engineerbetter.com>
2019-05-30 17:30:20 +0100

    enable compute api
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- tf/service_account.tf
+++ tf/service_account.tf
@@ -26,6 +26,12 @@ resource "google_service_account_key" "key" {
   service_account_id = "${google_service_account.concourse.name}"
 }
 
+resource "google_project_service" "project" {
+  service = "compute.googleapis.com"
+
+  disable_dependent_services = true
+}
+
 output "key" {
   value = "${google_service_account_key.key.private_key}"
 }
