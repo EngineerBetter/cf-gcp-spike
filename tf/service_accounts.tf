e430bbebcf4c164aa4320c08d7f350ca8a59003b
Colin Simmons <colin.simmons@engineerbetter.com>
2019-06-01 00:50:04 +0100

    randomise sa name
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

--- tf/service_accounts.tf
+++ tf/service_accounts.tf
@@ -1,5 +1,10 @@
+resource "random_pet" "account-name" {
+  length = 1
+  prefix = "concourse"
+}
+
 resource "google_service_account" "concourse" {
-  account_id   = "concourse"
+  account_id   = "${random_pet.account-name.id}"
   display_name = "Concourse"
 }
 
@@ -13,18 +18,4 @@ resource "google_project_iam_binding" "concourse-iam" {
 
 resource "google_service_account_key" "key" {
   service_account_id = "${google_service_account.concourse.name}"
-}
-
-resource "google_service_account" "blobstore" {
-  account_id   = "blobstore"
-  display_name = "Blobstore Service Account"
-}
-
-resource "google_service_account_key" "blobstore" {
-  service_account_id = "${google_service_account.blobstore.id}"
-}
-
-resource "google_project_iam_member" "blobstore_cloud_storage_admin" {
-  role   = "roles/storage.admin"
-  member = "serviceAccount:${google_service_account.blobstore.email}"
 }
